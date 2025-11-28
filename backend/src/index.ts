b7ea8e324697944df8bf6cc30d2d4f147043cf95
Lee Seunghyeon <lsh09130@gmail.com>
2025-11-28 15:00:38 +0900

    feat: initialize React + Express project w/ TS

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import express from "express";
+
+const app = express();
+const port = 4000;
+
+app.get("/", (req, res) => {
+  res.send("Express + TypeScript server running");
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
